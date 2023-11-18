16639086f88e7befbd7ead3352f3ca056157643a
Warish Raaz <106527502+warination@users.noreply.github.com>
2023-11-18 17:39:47 +0530

    third commit

---
+++ test/get-post/get-post.php
@@ -0,0 +1,36 @@
+<?php
+if(isset($_GET['name'])){
+    // echo  $_GET['name'];
+    print_r($_GET);
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get Post PHP</title>
+</head>
+
+<body>
+    <form method="GET" action="get-post.php">
+        <div>
+
+            <label for="">Mame</label>
+            <input type="text" name="name" id="">
+        </div>
+
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email" id="">
+        </div>
+
+        <input type="submit" value="submit">
+    </form>
+</body>
+
+</html>
